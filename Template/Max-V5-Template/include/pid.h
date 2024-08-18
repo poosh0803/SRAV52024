25fff14f01604ca8e889ff0ccb29901ee07fb553
DKpoosh <poosh0803@gmail.com>
2024-08-18 15:44:28 +1000

    fixed template

--- Template/Max-V5-Template/include/pid.h
+++ Template/Max-V5-Template/include/pid.h
@@ -1,10 +1,7 @@
-#ifndef F88165DE_9DD8_436A_ACB8_5E124F1A2FFF
-#define F88165DE_9DD8_436A_ACB8_5E124F1A2FFF
+
 void pid_Init();
 void pid_Ends();
 void turn_w_PID(vex::turnType dir, double target);
 void turn_to_heading(double targetHeading);
-void drive_w_PID(vex::directionType dir, double target, vex::distanceUnits units);
-
-
-#endif /* F88165DE_9DD8_436A_ACB8_5E124F1A2FFF */
+void drive_w_PID(vex::directionType dir, double target, vex::distanceUnits units, double speed = 80);
+void drive_for_time(vex::directionType direction, double timeLength);
--- Template/Max-V5-Template/src/pid.cpp
+++ Template/Max-V5-Template/src/pid.cpp
@@ -9,8 +9,10 @@
 using namespace vex;
 int turnANDdrive = 0;
 bool pidEnabled = false;
-double gearRatio = 48.0/84.0;
+double gearRatio = 72.0/48.0;
 double wheelTravel = 320;
+double driveSpeedCap = 100;
+double turnSpeedCap = 100;
 // Target positions for each motor (adjust these based on your task)
 double targetLeftDrivePosition = 0;
 double targetRightDrivePosition = 0;
@@ -23,18 +25,18 @@ double integralMotor2 = 0.0;
 double currentVelocityMotor1 = 0.0;
 double currentVelocityMotor2 = 0.0;
 // Turning PID parameters
-const double KpTurn = 0.09;
-const double KiTurn = 0.000028;
-const double KdTurn = 0.000001;
+const double KpTurn = 0.26;
+const double KiTurn = 0.000;
+const double KdTurn = -0.12;
 // Driving PID parameters
-const double KpDrive = 0.10;
-const double KiDrive = 0.000025;
-const double KdDrive = 0.000001;
+const double KpDrive = 0.23;
+const double KiDrive = 0.000;
+const double KdDrive = 0.35;
 const double timeStep = 10.0; // Time between each control loop update (in milliseconds)
 //Modify for turning
-const double veryCoolConstant = 5.605;
+const double veryCoolConstant = 2.2;
 //Modify for tolerance
-double tolerance = 30;
+double tolerance = 15;
 //Set Motor Position
 void printPosition(double L, double R)
 {
@@ -100,6 +102,7 @@ std::pair<turnType, double> determineTurnDirection(double currentHeading, double
 }
 void turn_to_heading(double targetHeading)
 {
+    turnANDdrive = 0;
     auto result = determineTurnDirection(Imu.heading(), targetHeading);
     turnType direction = result.first;
     double angleToTurn = result.second;
@@ -115,6 +118,7 @@ void turn_to_heading(double targetHeading)
 }
 void drive_for_time(directionType direction, double timeLength)
 {
+    turnANDdrive = 1;
     setMotorPos(0, 0);
     if(direction == forward)
     {
@@ -131,7 +135,7 @@ void drive_for_time(directionType direction, double timeLength)
 }
 void drive_w_PID(directionType dir, double target, distanceUnits units, double speed)
 {
-    //motor one spin = 320mm * 48/84 = 182.857
+    driveSpeedCap = speed;
     turnANDdrive = 1;
     if(units == vex::distanceUnits::cm)
     {
@@ -183,6 +187,11 @@ int pidLoop()
         {
             controlSignalMotor1 = calculateControlSignal(targetLeftDrivePosition, currentMotor1Position, prevErrorMotor1, integralMotor1,KpDrive, KiDrive, KdDrive);
             controlSignalMotor2 = calculateControlSignal(targetRightDrivePosition, currentMotor2Position, prevErrorMotor2, integralMotor2,KpDrive, KiDrive, KdDrive);
+            if(controlSignalMotor1 > driveSpeedCap) {controlSignalMotor1 = driveSpeedCap;}
+            else if(controlSignalMotor1 < -driveSpeedCap) {controlSignalMotor1 = -driveSpeedCap;}
+            if(controlSignalMotor2 > driveSpeedCap) {controlSignalMotor2 = driveSpeedCap;}
+            else if(controlSignalMotor2 < -driveSpeedCap) {controlSignalMotor2 = -driveSpeedCap;}
+            
         }
         LeftDrive.spin(forward, controlSignalMotor1, percent);
         RightDrive.spin(forward, controlSignalMotor2, percent);
@@ -203,8 +212,10 @@ void pid_Init()
     pidEnabled = true;
     LeftDrive.setPosition(0, degrees);
     LeftDrive.setMaxTorque(100,percent);
+    LeftDrive.setStopping(hold);
     RightDrive.setPosition(0, degrees);
     RightDrive.setMaxTorque(100,percent);
+    RightDrive.setStopping(hold);
     vex::task pidLoopTask(pidLoop);
     vex::task debugMonitorTask(debugMonitor);
 }
