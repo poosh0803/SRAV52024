7e7bc5b99d4ab5ff8c6c84b74910c71c918e4ead
YABABABAA <yabababa1213@gmail.com>
2024-08-27 17:51:51 +1000

    pid for 15sec auton

--- 58A/58A Auton/include/pid.h
+++ 58A/58A Auton/include/pid.h
@@ -2,6 +2,6 @@ void pid_Init();
 void pid_Ends();
 void turn_w_PID(vex::turnType dir, double target);
 
-void turnToHeading2(double targetHeading);
+void turnToHeading2(double targetHeading, double speed = 80);
 void driveFor2(vex::directionType dir, double target, vex::distanceUnits units, double speed=80);
 void drive_for_time(vex::directionType direction, double timeLength);
--- 58A/58A Auton/src/PlanB.cpp
+++ 58A/58A Auton/src/PlanB.cpp
@@ -6,4 +6,30 @@ using namespace vex;
 
 void autonPlanB()
 {
-}
+    mogo.set(true);
+    Imu.setHeading(0, degrees);
+    pid_Init();
+    driveFor2(reverse, 12, inches, 30);
+    turnToHeading2(93, 20);
+    driveFor2(reverse, 3, inches, 30);
+    Intake.setVelocity(40,percent);
+    Intake.spin(forward);
+
+    wait(1,sec);
+    Intake.stop();
+    driveFor2(forward, 10, inches);
+    Intake.setVelocity(100,percent);
+    Intake.spin(forward);
+    turnToHeading2(226);
+    driveFor2(reverse, 31.5, inches, 70);
+    mogo.set(false);
+
+    turnToHeading2(2);
+    driveFor2(forward, 24, inches);
+    turnToHeading2(85);
+    driveFor2(forward, 15, inches);
+    wait(1,sec);
+    turnToHeading2(13);
+    driveFor2(reverse, 35, inches);
+    pid_Ends();
+} 
