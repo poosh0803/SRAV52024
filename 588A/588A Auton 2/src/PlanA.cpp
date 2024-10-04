eab0db5aa371f83f806bc17e3ad60ba1b6cf4092
Xiong <bearalicious1@gmail.com>
2024-10-04 18:24:54 +1000

    tryin gmy best for auton
    ahahah hehehehe ahahaha ehehehe(resusing old code) rehhehehe

--- 588A/588A Auton 2/src/PlanA.cpp
+++ 588A/588A Auton 2/src/PlanA.cpp
@@ -17,24 +17,27 @@ void autonPlanA()
     Lift.spinToPosition(850, degrees);
     Lift.spinToPosition(1100, degrees, false);
 
-    setMotorPos(360, 0);
+    setMotorPos(353, 0);   //350 if doesnt work
+    drive_w_PID(forward, 3.4, inches);
 
     Lift.spinToPosition(850, degrees, false);
     Lift.spinToPosition(0, degrees);
     wait(0.001,sec);
-    drive_w_PID(reverse, 3, inches);
+    drive_w_PID(reverse, 2, inches);
     turn_to_heading(127);
 
     wait(1,sec);
+    drive_w_PID(forward, 5, inches);
     Intake.setVelocity(40,percent);
-    Intake.spin(reverse);
+    Intake.spin(reverse); 
     Intake.stop();
-    drive_w_PID(forward, 10, inches);
+    drive_w_PID(forward, 4, inches);
     Intake.setVelocity(100,percent);
     Intake.spin(forward);
     turn_to_heading(226);
-    drive_w_PID(reverse, 31, inches, 70);
+    drive_w_PID(reverse, 35, inches, 70);
     mogoGODOWN();       //clip mogo(go down)
+    wait(0.6,sec);
 
     turn_to_heading(2);
     drive_w_PID(forward, 24.5, inches);   //intake 1 ring
