16752e09994c7c456a99f5b163ff60e4d859bbef
Ella <bearalicious1@gmail.com>
2024-11-29 17:01:34 +1100

    a,b not tested

--- 588A/588A Auton 2/src/PlanB.cpp
+++ 588A/588A Auton 2/src/PlanB.cpp
@@ -27,11 +27,11 @@ void autonPlanB()
    turn_to_heading(355);
    intakeSTART();
    drive_w_PID(forward, 28, inches, 50);
-   turn_to_heading(280);
-   turn_to_heading(280);
-   drive_w_PID(forward, 10.8, inches, 50);
-   wait(100,msec);
-   drive_w_PID(reverse, 10.8, inches, 30);
+   turn_to_heading(280);                   //
+   turn_to_heading(280);                    //
+   drive_w_PID(forward, 10.8, inches, 50); //
+   wait(100,msec);                          //
+   drive_w_PID(reverse, 10.8, inches, 30);  //
    turn_to_heading(250);
    turn_to_heading(250);
    drive_w_PID(forward, 10.7, inches, 50);
